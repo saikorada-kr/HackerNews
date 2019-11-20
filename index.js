f8096c1e5dd8257389adf794d804afa49b570e83
saikorada-kr <saikorada.kr@gmail.com>
2019-11-21 00:32:15 +0530

    adding html in new file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const path= require('path');
 const app = express();
 
 app.get('/',(req, res) => {
-res.send('<h1>Hello world</h1>');
+res.sendFile(path.join(__dirname,'public','index.html'));
 
 });
 
